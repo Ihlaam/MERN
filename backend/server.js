1a69623623386ed75e61720a59f0d1d552ce9134
Ihlaam <47963182+Ihlaam@users.noreply.github.com>
2024-08-29 20:50:34 +0200

    https enabled

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,9 @@ const mongoose = require('mongoose')
 const helmet = require('helmet')
 const fs = require('fs');
 const https = require('https');
+const path = require('path');
 const bookRoutes = require('./routes/bookRouter')
 const userRoutes = require('./routes/userRouter')
-
-//https/tls
-const credentials = {
-     key: fs.readFileSync('/etc/ssl/private/private.key'), 
-     cert: fs.readFileSync('/etc/ssl/private/server.cert') };
 
 //create express app by invoking a function
 const app = express()
@@ -25,29 +21,35 @@ app.use((req, res, next) => {
     console.log(req.path, req.method) 
     next()
 })
-app.use(
-    helmet({
-      xFrameOptions: { action: "deny" },
-    }),
-  );
+// app.use(
+//     helmet({
+//       xFrameOptions: { action: "deny" },
+//     }),
+//   );
 
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      frameAncestors: ["self"]
-    }
-  }));
+// app.use(helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       frameAncestors: ["self"]
+//     }
+//   }));
 
 //routes
 app.use('/api/books', bookRoutes)
 app.use('/api/user', userRoutes)
 
 
+
+const sslServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+}, app)
+
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        //create https server
-        https.createServer(credentials, app).listen(process.env.PORT, () => {
+        //create http server
+        sslServer.listen(process.env.PORT, () => {
             console.log(`HTTPS Server running on port 4000`)
         })
     })
